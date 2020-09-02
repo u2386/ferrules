4ea3d34300c67aeb428fa3e6802bf26888beff63
u2386 <hugo.cavan2386@gmail.com>
2020-09-02 17:25:19 +0800

    feat: Add a weather rule example

---
+++ example/weather/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/u2386/ferrules/pkg/ferrules"
+)
+
+var rules = ferrules.RuleSet(ferrules.RuleBuilder().
+	Given(func(facts ferrules.Facts) bool {
+		rain, _ := facts.Get("rain")
+		return rain.Value.(bool)
+	}).
+	Will(func(facts ferrules.Facts) {
+		fmt.Println("It's rain, take an umbrella!")
+	}).
+	Then(func(facts ferrules.Facts) {
+		facts.Put("umbrella", "open")
+	}).
+	Priority(1).
+	WithName("weather rule").
+	WithDescription("if it rains then take an umbrella").
+	Build())
+
+func main() {
+	facts := ferrules.FactSet()
+	facts.Put("rain", true)
+
+	engine := ferrules.DefaultRuleEngine{}
+	engine.Fire(rules, facts)
+	fmt.Println(facts)
+}
